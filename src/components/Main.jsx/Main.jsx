c14fd97acf8d36ed6a6883ba998ec6b17a6f9fb0
DanielMarinBeltran <danielmarin998@hotmail.com>
2022-05-11 19:36:17 -0500

    commit inicial

---
+++ src/components/Main.jsx/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ParticlesBackground from '../Particles/ParticlesBackground'
+import './main.scss'
+
+function Main() {
+  return (
+    <div className='main'>
+      {/* <ParticlesBackground /> */}
+      <div className='prueba'>Hola</div>
+    </div>
+  )
+}
+
+export default Main
